4b57ad12096624f7321cd300f56682d3930b974f
xyzabhi <abhinavkumar8083@gmail.com>
2020-06-24 20:14:09 +0530

    Homepage successfully created

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,41 +1,11 @@
 import React from "react";
 import "./homepage.scss";
+import DirrectoryList from "./components/directory-list/DirrectoryList.components";
 
 export default function HomePage() {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <DirrectoryList />
     </div>
   );
 }
---
+++ src/components/menu-list/MenuList.components.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./MenuList.styles.scss";
+
+export default function MenuList({ title, imageUrl, size }) {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+}
